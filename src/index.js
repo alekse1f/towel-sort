c8fb98f185cd680bb72228c51b4437ce53380b8c
alekse1f <67468043+alekse1f@users.noreply.github.com>
2021-03-14 20:24:14 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(arr) {
+    let result = [];
+
+    for (i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            Array.prototype.push.apply(result, arr[i]);
+        } else {
+            const arrR = arr[i].reverse();
+            Array.prototype.push.apply(result, arrR);
+        }
+    }
+
+    return result;
 }
